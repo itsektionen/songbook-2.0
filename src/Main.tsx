64c63169cdb8c870e5272bd4d530d889c8271148
Leonard Lindgren <lindgrenleonard@gmail.com>
2026-06-12 01:47:11 +0200

    Remove uncessecary toast import

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import { BookmarksProvider } from './context/bookmarksContext';
 import { SearchProvider } from './context/searchContext';
